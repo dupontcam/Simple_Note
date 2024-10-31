dcc756389d05482488200a321a2bdd8c843d5011
dupontcam <125046483+dupontcam@users.noreply.github.com>
2024-10-30 22:29:40 -0300

    Entity Note

---
+++ app/src/main/java/br/com/cursoandroid/simplenote/model/Note.java
@@ -0,0 +1,68 @@
+package br.com.cursoandroid.simplenote.model;
+
+public class Note {
+
+    private Long ID;
+    private String title;
+    private String description;
+    private String date;
+    private String time;
+
+    public Note() {
+    }
+
+    public Note(String title, String description, String date, String time) {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.time = time;
+    }
+
+    public Note(long id, String title, String description, String date, String time) {
+        this.ID = id;
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.time = time;
+    }
+
+    public long getID() {
+        return ID;
+    }
+
+    public void setID(long ID) {
+        this.ID = ID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
