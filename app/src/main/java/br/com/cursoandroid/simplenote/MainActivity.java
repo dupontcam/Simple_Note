8ed379a54c2c99ab83c44324535a342582eb01ae
dupontcam <125046483+dupontcam@users.noreply.github.com>
2024-10-30 23:16:07 -0300

    Main activity

--- app/src/main/java/br/com/cursoandroid/simplenote/MainActivity.java
+++ app/src/main/java/br/com/cursoandroid/simplenote/MainActivity.java
@@ -1,14 +1,39 @@
 package br.com.cursoandroid.simplenote;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.cursoandroid.simplenote.adapter.Adapter;
+import br.com.cursoandroid.simplenote.database.NoteDatabase;
+import br.com.cursoandroid.simplenote.model.Note;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean isGridLayout = false;
+    Toolbar toolbar;
+    RecyclerView recyclerView;
+    Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +45,142 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        toolbar = findViewById(R.id.materialToolbar);
+        setSupportActionBar(toolbar);
+
+        NoteDatabase db = new NoteDatabase(this);
+        List<Note> notes = db.getNotes();
+        if (notes == null) {
+            notes = new ArrayList<>();
+        }
+
+        // Encontra o FloatingActionButton pelo ID
+        FloatingActionButton fab = findViewById(R.id.fabAdd);
+
+        // Define o OnClickListener para o FAB
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Inicia a Activity AddNote
+                startActivity(new Intent(MainActivity.this, AddNote.class));
+            }
+        });
+
+        recyclerView = findViewById(R.id.listOfNotes);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        isGridLayout = sharedPreferences.getBoolean("isGridLayout", false); // false é o valor padrão
+
+        adapter = new Adapter(notes, isGridLayout);
+
+        recyclerView.setAdapter(adapter);
+
+        // Define o layout inicial como lista
+        setLayoutManager();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        search(menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+
+        if (item.getItemId() == R.id.list) {
+            isGridLayout = false;
+            invalidateOptionsMenu(); // Força a atualização do menu
+            setLayoutManager();
+        } else if (item.getItemId() == R.id.grid) {
+            isGridLayout = true;
+            invalidateOptionsMenu(); // Força a atualização do menu
+            setLayoutManager();
+        }
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("isGridLayout", isGridLayout);
+        editor.apply();
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    public void search(Menu menu) {
+
+        // Referência ao item de adicionar
+        MenuItem addItem = menu.findItem(R.id.fabAdd);
+
+        // Referência ao item de pesquisa
+        MenuItem searchItem = menu.findItem(R.id.app_bar_search);
+
+        if (searchItem != null) { // Verificar se o item de menu existe
+            SearchView searchView = (SearchView) searchItem.getActionView(); // Criar o SearchView
+            searchView.setQueryHint("Pesquise anotações...");
+
+            // Listener para capturar o texto de busca
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    performSearch(query);
+                    return true;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    performSearch(newText);
+                    return true;
+                }
+            });
+
+            // Listener para quando a pesquisa é iniciada
+            searchView.setOnSearchClickListener(v -> {
+                addItem.setVisible(false);
+
+            });
+
+            // Listener para quando a pesquisa é finalizada
+            searchView.setOnCloseListener(() -> {
+                addItem.setVisible(true);
+                invalidateOptionsMenu(); // Força a recriação do menu para garantir o posicionamento correto
+                return false;
+            });
+        }
+    }
+
+    private void performSearch(String query) {
+
+        // Obter as anotações do banco de dados com base na consulta
+        NoteDatabase db = new NoteDatabase(this);
+        List<Note> searchResults = db.searchNotes(query);
+
+        // Atualizar o Adapter com os resultados da pesquisa
+        adapter.setNotes(searchResults);
+        adapter.notifyDataSetChanged();
+    }
+
+    // Atualizar o menu com o ícone correto visível
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        menu.findItem(R.id.grid).setVisible(!isGridLayout);
+        menu.findItem(R.id.list).setVisible(isGridLayout);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    // Método para definir o layout do RecyclerView
+    private void setLayoutManager() {
+        if (isGridLayout) {
+            recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        } else {
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        }
+        adapter.setGridLayout(isGridLayout);
+        recyclerView.getRecycledViewPool().clear(); // Limpar o cache (opcional)
     }
 }
