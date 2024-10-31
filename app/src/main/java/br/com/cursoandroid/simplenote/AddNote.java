cfea5897352a6006fc8925080efeed2423d16271
dupontcam <125046483+dupontcam@users.noreply.github.com>
2024-10-31 08:40:49 -0300

    Add Note

---
+++ app/src/main/java/br/com/cursoandroid/simplenote/AddNote.java
@@ -0,0 +1,104 @@
+package br.com.cursoandroid.simplenote;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Calendar;
+
+import br.com.cursoandroid.simplenote.database.NoteDatabase;
+import br.com.cursoandroid.simplenote.model.Note;
+
+public class AddNote extends AppCompatActivity {
+
+    Toolbar toolbar;
+    EditText noteTitle, noteDetails;
+    String todaysDate, currentTime;
+    Calendar c;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_add_note);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        toolbar = findViewById(R.id.materialToolbar2);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("New Note");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        noteTitle = findViewById(R.id.noteTitle);
+        noteDetails = findViewById(R.id.noteDetails);
+
+        //get current date and time
+        c = Calendar.getInstance();
+        todaysDate = c.get(Calendar.DAY_OF_MONTH) + "/" + (c.get(Calendar.MONTH) + 1) + "/" + c.get(Calendar.YEAR);
+        currentTime = pad(c.get(Calendar.HOUR_OF_DAY)) + ":" + pad(c.get(Calendar.MINUTE));
+
+        Log.d("date", todaysDate);
+        Log.d("time", currentTime);
+    }
+
+    private String pad(int i) {
+        if(i < 10)
+            return "0" + i;
+        return String.valueOf(i);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_save, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+
+        NoteDatabase db = new NoteDatabase(this);
+        Note note = new Note();
+
+        if (item.getItemId() == R.id.save) {
+            if (noteTitle.getText().toString().isEmpty()) {
+                noteTitle.setError("O título não pode estar vazio.");
+                return false;
+            }
+            if (noteDetails.getText().toString().isEmpty()) {
+                noteDetails.setError("A descrição não pode estar vazia.");
+                return false;
+            }
+            note.setTitle(noteTitle.getText().toString());
+            note.setDescription(noteDetails.getText().toString());
+            note.setDate(todaysDate);
+            note.setTime(currentTime);
+            note = new Note(noteTitle.getText().toString(), noteDetails.getText().toString(), todaysDate, currentTime);
+            db.addNote(note);
+            Toast.makeText(this, "Anotação salva.", Toast.LENGTH_SHORT).show();
+            goToMain();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void goToMain() {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+}
---
+++ app/src/main/java/br/com/cursoandroid/simplenote/adapter/Adapter.java
@@ -0,0 +1,124 @@
+package br.com.cursoandroid.simplenote.adapter;
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import br.com.cursoandroid.simplenote.R;
+import br.com.cursoandroid.simplenote.UpdateNote;
+import br.com.cursoandroid.simplenote.model.Note;
+
+public class Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    static List<Note> notes;
+    private boolean isGridLayout;
+
+    public Adapter(List<Note> notes, boolean isGridLayout) {
+        this.isGridLayout = isGridLayout;
+        this.notes = notes;
+    }
+
+    public void setGridLayout(boolean isGridLayout) {
+        this.isGridLayout = isGridLayout;
+        notifyDataSetChanged(); // Atualiza a exibição
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        if (isGridLayout) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_grid_view, parent, false);
+            return new GridViewHolder(view); // Criar GridViewHolder para grade
+        } else {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_list_view, parent, false);
+            return new ListViewHolder(view); // Criar ListViewHolder para lista
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+        if (holder instanceof GridViewHolder) {
+            GridViewHolder gridHolder = (GridViewHolder) holder;
+            gridHolder.noteTitle.setText(notes.get(position).getTitle());
+            gridHolder.noteDescription.setText(notes.get(position).getDescription());
+        } else if (holder instanceof ListViewHolder) {
+            ListViewHolder listHolder = (ListViewHolder) holder;
+            listHolder.noteTitle.setText(notes.get(position).getTitle());
+            listHolder.nDate.setText(notes.get(position).getDate());
+            listHolder.nTime.setText(notes.get(position).getTime());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public void setNotes(List<Note> searchResults) {
+        this.notes = searchResults;
+    }
+
+    public static class ListViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public TextView noteTitle, nDate, nTime;
+
+        public ListViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            noteTitle = itemView.findViewById(R.id.noteTitle);
+            nDate = itemView.findViewById(R.id.nDate);
+            nTime = itemView.findViewById(R.id.nTime);
+
+            itemView.setOnClickListener(this);
+        }
+
+
+        @Override
+        public void onClick(View view) {
+            int position = getAdapterPosition();
+            if (position != RecyclerView.NO_POSITION) {
+                Note clickedNote = notes.get(position);
+                Intent intent = new Intent(view.getContext(), UpdateNote.class);
+                intent.putExtra("noteId", clickedNote.getID()); // Passe o ID ou outros dados da anotação
+                intent.putExtra("noteTitle", clickedNote.getTitle());
+                intent.putExtra("noteDescription", clickedNote.getDescription());
+                view.getContext().startActivity(intent);
+            }
+        }
+    }
+
+    public static class GridViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public TextView noteTitle;
+        public TextView noteDescription;
+
+        public GridViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            noteTitle = itemView.findViewById(R.id.noteTitle); // Inicializar noteTitle
+            noteDescription = itemView.findViewById(R.id.noteDescription); // Inicializar noteDescription
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int position = getAdapterPosition();
+            if (position != RecyclerView.NO_POSITION) {
+                Note clickedNote = notes.get(position);
+                Intent intent = new Intent(view.getContext(), UpdateNote.class);
+                intent.putExtra("noteId", clickedNote.getID()); // Passe o ID ou outros dados da anotação
+                intent.putExtra("noteTitle", clickedNote.getTitle());
+                intent.putExtra("noteDescription", clickedNote.getDescription());
+                view.getContext().startActivity(intent);
+            }
+        }
+    }
+}
